29d1ec30cf1fe5ef288af7eeff41df924c6d77d6
hencci <hencci20@gmail.com>
2025-07-10 14:21:01 +0100

    Using ternary operator to conditionally render an element

---
+++ src/RenderingTecniques/ConditionalRendering.jsx
@@ -0,0 +1,22 @@
+// One way to conditionally render an element is with a ternary operator, using a boolean value to decide what to render:
+
+function List(props) {
+    return (
+        <ul>
+            {props.animals.map((animal) => {
+                return animal.startsWith("L") ? <li key={animal}>{animal}</li> : null;
+            })}
+        </ul>
+    );
+}
+
+function App() {
+    const animals = ["Lion", "Cow", "Snake", "Lizard"];
+
+    return (
+        <div>
+            <h1>Animals: </h1>
+            <List animals={animals} />
+        </div>
+    );
+}
